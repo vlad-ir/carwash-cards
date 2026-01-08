aebba2319aa72b0f5bbdc650e65b0362f34be3ee
Otiby <vlad_ir@tut.by>
2026-01-08 17:29:06 +0300

    Add log to csv-import command

--- app/Console/Commands/CarwashCsvImportStatsCommand.php
+++ app/Console/Commands/CarwashCsvImportStatsCommand.php
@@ -17,6 +17,13 @@ class CarwashCsvImportStatsCommand extends Command
         $service = new CarwashCsvImportService();
         $processed = $service->processCsvFiles();
 
+        // Логируем вызов
+        $logFile = storage_path('logs/cron_debug.log');
+        $message = date('Y-m-d H:i:s') . " - Cron команда вызвана\n";
+        $message .= " - Обработано файлов: {$processed}\n";
+        file_put_contents($logFile, $message, FILE_APPEND);
+
         $this->info("Обработано файлов: {$processed}");
     }
 }
+
